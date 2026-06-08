8739fcfd25b40641f853694ed6bb93e16dfda01a
sharfrasaqsan <sharfrasaqsan@gmail.com>
2026-06-08 12:46:11 +0530

    dbupdates

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('DB_HOST', 'acela.proxy.rlwy.net');
 define('DB_NAME', 'railway');
 define('DB_USER', 'root');
 define('DB_PASS', 'gSPeixzelMWXeyEGYDTTOUWUAUqPXMLn');
-define('DB_PORT', '3306');
+define('DB_PORT', '27975');
 
 // Workflow Webhook Configuration
 define('WORKFLOW_WEBHOOK_URL', 'https://services.leadconnectorhq.com/hooks/4PU6SQmWMZDQmB4PgBIW/webhook-trigger/06a7dc36-2aff-43a9-89fe-61d28cf45493');
